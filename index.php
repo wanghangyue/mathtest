6f9f4c20a24ca3a7601f689257060af6176ca423
wanghangyue <learn957@163.com>
2014-01-02 14:01:00 +0800

    the first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>todayreviewshow</title>
+<meta http-equiv="Content-Type" content="text/html; charset=gbk" />
+<script type="text/javascript" src="mathjax.js"></script>
+<script type="text/javascript" src="mathjax2.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+</head>
+<body>
+<form action="mathtest.php" method="post">
+  <textarea name="mathtest" cols ="50" rows = "30"></textarea>
+  <input type="reset" value="RESET"> 
+  <input type="submit" value="Submit" />
+</form>
+</body>
+</html>
---
+++ mathjax.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+MathJax.Hub.Config({
+extensions: ["tex2jax.js"],
+jax: ["input/TeX", "output/HTML-CSS"],
+tex2jax: {
+inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+processEscapes: true
+},
+"HTML-CSS": { availableFonts: ["TeX"] }
+});
